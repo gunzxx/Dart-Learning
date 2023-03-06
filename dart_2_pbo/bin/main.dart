22712a793f6466bf464eeb67abcb1071c048373d
gunzxx <ggm5850@gmail.com>
2023-03-06 21:17:05 +0700

    Encapsulation - getter dan setter

--- dart_2_pbo/bin/main.dart
+++ dart_2_pbo/bin/main.dart
@@ -4,15 +4,17 @@ import 'package:dart_2_pbo/dart_2_encapsulation.dart';
 void main(List<String> arguments) {
   PersegiPanjang k1, k2;
 
+  // Get set biasa
   k1 = new PersegiPanjang(); // tidak perlu menggunakan new
   k1.setPanjang(3);
   k1.setLebar(3);
-  double luask1 = k1.luas();
+  double luask1 = k1.hitungLuas();
 
+  // Getter dan setter method
   k2 = PersegiPanjang();
   k2.panjang = 5;
   k2.lebar = 5;
-  double luask2 = k2.luas();
+  double luask2 = k2.luas;
 
   print("Luas kotak 1 = $luask1");
   print("Luas kotak 2 = $luask2");
