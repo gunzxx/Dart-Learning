9d898435abb146a2efb2d74752be863c94da8f68
gunzxx <ggm5850@gmail.com>
2023-03-12 09:56:02 +0700

    inheritance

--- dart_2_pbo/bin/main.dart
+++ dart_2_pbo/bin/main.dart
@@ -1,24 +1,26 @@
-import 'package:dart_2_pbo/dart_3_inheritance.dart';
+// import 'package:dart_2_pbo/dart_3_inheritance.dart';
 // import 'dart:io';
 
 void main(List<String> arguments) {
-  Hero miya = Hero();
-  miya.healthpoint = 100;
+  String nama;
+  nama = "123";
+  // Hero miya = Hero();
+  // miya.healthpoint = 100;
 
-  Monster monster = Monster();
-  Monster kucing = MonsterIkan();
-  Monster serigala = MonsterSerigala();
-  List<Monster> listOfMonster = [];
+  // Monster monster = Monster();
+  // Monster kucing = MonsterIkan();
+  // Monster serigala = MonsterSerigala();
+  // List<Monster> listOfMonster = [];
 
-  listOfMonster.add(MonsterSerigala());
-  listOfMonster.add(MonsterIkan());
-  listOfMonster.add(MonsterSerigala());
+  // listOfMonster.add(MonsterSerigala());
+  // listOfMonster.add(MonsterIkan());
+  // listOfMonster.add(MonsterSerigala());
 
-  print((kucing as MonsterIkan).swim());
+  // print((kucing as MonsterIkan).swim());
 
-  for (Monster m in listOfMonster) {
-    if (m is MonsterSerigala) {
-      print(m.au());
-    }
-  }
+  // for (Monster m in listOfMonster) {
+  //   if (m is MonsterSerigala) {
+  //     print(m.au());
+  //   }
+  // }
 }
