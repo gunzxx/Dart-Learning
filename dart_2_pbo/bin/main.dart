24c57abe2e6596fe9799d9f2492f5367156c23e3
gunzxx <ggm5850@gmail.com>
2023-03-13 21:33:12 +0700

    constructor

--- dart_2_pbo/bin/main.dart
+++ dart_2_pbo/bin/main.dart
@@ -1,7 +1,10 @@
+import 'package:dart_2_pbo/dart_6_constructor.dart';
 import 'dart:io';
 
 void main(List<String> arguments) {
   var input = stdin.readLineSync();
-
   print('kamu mencetak = $input');
+
+  Person n = Person(name: "Joko");
+  print(n.name);
 }
