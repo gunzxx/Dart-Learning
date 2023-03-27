ff267561a25be56ce519a55c3cb59d1b42725870
gunzxx <ggm5850@gmail.com>
2023-03-27 11:47:21 +0700

    foreach & sort

--- dart_2_pbo/bin/main.dart
+++ dart_2_pbo/bin/main.dart
@@ -1,23 +1,33 @@
-import 'package:meta/meta.dart';
-// import 'package:dart_2_pbo/dart_12_enum_cascade.dart';
-// import 'dart:io';
-
 void main(List<String> args) {
-  Person p;
-  try {
-    p = Person(name: null);
-  } catch (e) {
-    print(4);
-  }
+  List<Person> persons = [
+    Person("Admin", 12),
+    Person("User", 31),
+    Person("Admin", 23),
+    Person("Merchant", 32),
+    Person("Admin", 24),
+    Person("User", 46),
+    Person("Merchant", 41),
+    Person("User", 13),
+    Person("Merchant", 14),
+  ];
 
-  print(p.age);
+  // persons.sort((p1, p2) => p1.umur.compareTo(p2.umur));
+  persons.sort((p1, p2) {
+    if (p1.role.compareTo(p2.role) != 0) {
+      return p1.role.compareTo(p2.role);
+    } else {
+      return p1.umur.compareTo(p2.umur);
+    }
+  });
+
+  persons.forEach((element) {
+    print(element.role + " = " + element.umur.toString());
+  });
 }
 
 class Person {
-  final String name;
-  final int age;
+  final String role;
+  final int umur;
 
-  Person({@required this.name, this.age = 0}) {
-    assert(name != null, "Must a string name");
-  }
+  Person(this.role, this.umur);
 }
---
+++ dart_2_pbo/bin/main_13_required_assert_trycatch.dart
@@ -0,0 +1,23 @@
+import 'package:meta/meta.dart';
+// import 'package:dart_2_pbo/dart_12_enum_cascade.dart';
+// import 'dart:io';
+
+void main(List<String> args) {
+  Person p;
+  try {
+    p = Person(name: null);
+  } catch (e) {
+    print(4);
+  }
+
+  print(p.age);
+}
+
+class Person {
+  final String name;
+  final int age;
+
+  Person({@required this.name, this.age = 0}) {
+    assert(name != null, "Must a string name");
+  }
+}
